029faa4aef2a7d3e810b8c64582839f8d728e5a1
thewanionly <pelloani@gmail.com>
2024-02-20 04:53:10 +0800

    feat: display blog post content using mdx

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import BlogHero from "@/components/BlogHero";
+import { loadBlogPost } from '@/helpers/file-helpers';
 
-import styles from "./postSlug.module.css";
+import BlogHero from '@/components/BlogHero';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
-function BlogPost() {
+import styles from './postSlug.module.css';
+
+async function BlogPost({ params: { postSlug } }) {
+  const { frontmatter, content } = await loadBlogPost(postSlug);
+
+  const { title, publishedOn } = frontmatter;
   return (
     <article className={styles.wrapper}>
-      <BlogHero title="Example post!" publishedOn={new Date()} />
+      <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of the elements
-          created from the blog post in this spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
