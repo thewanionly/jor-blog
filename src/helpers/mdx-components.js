ec214771e02b8426368acd15c6fda79981f59e7b
thewanionly <pelloani@gmail.com>
2024-02-29 04:01:32 +0800

    feat: add CircularColorsDemo in component mapping

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,7 +1,9 @@
 import CodeSnippet from '@/components/CodeSnippet';
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+import CircularColorsDemo from '@/components/CircularColorsDemo';
 
 export const COMPONENT_MAP = {
   pre: CodeSnippet,
-  DivisionGroupsDemo
+  DivisionGroupsDemo,
+  CircularColorsDemo
 };
